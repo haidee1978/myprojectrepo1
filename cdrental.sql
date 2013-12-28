bc8f241aaadd5d867a9b2d53156174d89573de3d
akang mole <haidee.com>
2013-12-27 19:31:44 -0800

    [NEW]cd for rent

---
+++ cdrental.sql
@@ -0,0 +1,10 @@
+create table cd_rent (
+cd_ID int not null,
+custo_ID int not null,
+custo_name char[20],
+address char[50],
+contact_num int not null,
+rent_date date not null,
+incharge char[20],
+Primary key (custo_ID),
+foreign key (cd_ID)references cd_details (cd_id));
