f921ea7e7a5fce32dc8c86d4807ed9e21f45030b
akang mole <haidee.com>
2013-12-28 13:19:16 -0800

    [old]cdreturn

--- cdreturn.sql
+++
@@ -1,7 +0,0 @@
-create table return_cd(
-cd_ID int not null,
-cd_status text not null,
-returned_date date not null,
-incharge text not null,
-primary key (cd_ID),
-foreign key (cd_ID)references cd_details(cd_ID));
