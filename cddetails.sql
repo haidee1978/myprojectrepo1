8f9a0d4fa027b0a445a785aa0b2313e68815ca3e
akang mole <haidee.com>
2013-12-27 23:05:10 -0800

    [NEW]details

--- cddetails.sql
+++ cddetails.sql
@@ -1,7 +1,7 @@
 create table cd_details (
 	cd_id int Primary Key not null,
-	cd_title char [50],
-	genre char[20],
+	cd_title char (50),
+	genre char(20),
 	amount int not null,
 	nocopies int not null,
 	manufac_date date
